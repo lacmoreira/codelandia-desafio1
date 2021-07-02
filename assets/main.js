e653c20e978f5edb92689ebf45933636abaf119f
lusomoreira <lusocmoreira@gmail.com>
2021-07-01 21:32:26 -0300

    finalizacao main.js

--- assets/main.js
+++ assets/main.js
@@ -10,3 +10,9 @@ ScrollReveal().reveal('.container', { delay: 200 });
 ScrollReveal().reveal('.data', { delay: 400 });
 ScrollReveal().reveal('.title', { delay: 500 });
 ScrollReveal().reveal('.texto', { delay: 600 });
+
+
+/**
+ * 
+ * falta implementar botão curtir
+ */
